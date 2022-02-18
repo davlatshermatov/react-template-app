8528c158e2a946e84e2659cf5108a4b441371649
Davlat Shermatov <davlatshermatov111@gmail.com>
2022-02-18 10:58:43 +0500

    added theme provider

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Theme from "./utils/Theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Theme>
+        <App />
+      </Theme>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
